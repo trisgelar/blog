5c7de4fcd43a400dd1672d038c41e70224cf5296
Trisna Gelar <trisgelar@gmail.com>
2019-04-22 12:12:34 +0700

    Chapter 1 Part 3

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,13 @@
 from django import forms
+from .models import Comment
 
 class EmailPostForm(forms.Form):
-	name = forms.Charfield(max_length=25)
+	name = forms.CharField(max_length=25)
 	email = forms.EmailField()
 	to = forms.EmailField()
-	comments = forms.Charfield(required=false, widget=forms.Textarea)
+	comments = forms.CharField(required=False, widget=forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		fields = ('name', 'email', 'body')
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'taggit',
 
     'django.contrib.admin',
     'django.contrib.auth',
